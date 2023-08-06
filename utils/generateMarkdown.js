86beea94160e9ef511847f98514999bee27c15a6
Ricky Carter <randolphcarter5@gmail.com>
2023-08-06 17:46:43 -0400

    creataed functions with conditions relating to License: badge, link, and section to  append to README file.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,31 +1,78 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const license = "MIT";
+
+function renderLicenseBadge(license) {  
+      const licenseBadges = {
+        'MIT': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+        'Apache-2.0': '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',   
+      }
+      return licenseBadges[license] || '';
+    
+};
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+      const licenseLinks = {
+        'MIT': 'https://opensource.org/licenses/MIT',
+        'Apache-2.0': 'https://opensource.org/licenses/Apache-2.0'
+      } 
+    return licenseLinks[license] || '';
+};
+
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title} 
-          ## ${data.description}
-          ## ${date.TableOfContents}
-          ## ${Installation}
-          ## ${Usage}
-          ## ${Credits}
-          ## ${License}          
-          ## ${Constributing}          
-          ## ${Tests}          
-          ## ${Questions}          
-        `;
+function renderLicenseSection(license) {
+      const licenseSections = {
+        'MIT': `MIT License
+          This project is licensed under the MIT License - see the [LICENSE](https://opensource.org/licenses/MIT) page for details.`,
+        'Apache-2.0': `Apache License 2.0
+          This project is licensed under the Apache License, Version 2.0 - see the [LICENSE](https://opensource.org/licenses/Apache-2.0) page for details.`
+      }
+    return licenseSections[license] || '';
 };
 
-module.exports = generateMarkdown;
+console.log(renderLicenseBadge(license));
+console.log(renderLicenseLink(license));
+console.log(renderLicenseSection(license));
+
+// TODO: Create a function to generate markdown for README
+// const data = [
+//   {title: "Title"},
+//   {description: "Description"},
+//   {tableOfContents: "Table of Contents"},
+//   {installation: "Installation"},
+//   {usage: "Usage"},
+//   {credits: "Credits"},
+//   {license: "License"},
+//   {contributing: "Constributing"},
+//   {tests:"Tests"},
+//   {questions: "Questions"}
+// ]
+
+// function generateMarkdown(data) {
+//   for(let prop of data) {
+//     console.log(prop);
+//     return `# ${data[0].title} 
+//             ## ${data[1].description}
+//             ## ${data[2].tableOfContents}
+//             ## ${data[3].installation}
+//             ## ${data[4].usage}
+//             ## ${data[5].credits}
+//             ## ${data[6].license}          
+//             ## ${data[7].constributing}          
+//             ## ${data[8].tests}          
+//             ## ${data[9].questions}          
+//           `;
+//   }
+// };
+
+// generateMarkdown(...data);
+// module.exports = generateMarkdown;
 
 
 
