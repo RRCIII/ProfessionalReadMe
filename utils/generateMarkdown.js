febbc0f29b3108395027c3028b612fdf8a246f7b
Ricky Carter <randolphcarter5@gmail.com>
2023-08-16 14:51:38 -0400

    troubleshooting README file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,8 +13,8 @@ ${confirm.confirmInstall(data.install)}
 ${confirm.confirmUsage(data.usage)}
 ${confirm.confirmContribute(data.constribute)}
 ${confirm.confirmTesting(data.test)}
-*[License](#license)
-*[Questions](#questions)
+n\*[License](#license)
+n\*[Questions](#questions)
 
 
 ${confirm.createInstall(data.install)}
